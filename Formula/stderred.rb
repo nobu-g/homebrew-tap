cb190542f342d42629710f3fb4653fde303c6be8
nobu-g <ueda-n127@hotmail.co.jp>
2022-04-17 21:39:26 +0900

    fix

--- Formula/stderred.rb
+++ Formula/stderred.rb
@@ -2,7 +2,7 @@ class Stderred < Formula
   desc "Stderr in red"
   homepage "https://github.com/sickill/stderred"
   url "https://github.com/sickill/stderred.git"
-  version 'HEAD'
+  version "HEAD"
 
   depends_on "cmake" => :build
 
@@ -10,9 +10,10 @@ class Stderred < Formula
     system "make"
   end
 
-  def caveats; <<~EOS
-    To use stderred export the following before running a command
-      export DYLD_INSERT_LIBRARIES="#{HOMEBREW_PREFIX}/lib/libstderred.dylib${DYLD_INSERT_LIBRARIES:+:DYLD_INSERT_LIBRARIES}"
+  def caveats
+    <<~EOS
+      To use stderred export the following before running a command
+        export DYLD_INSERT_LIBRARIES="#{HOMEBREW_PREFIX}/lib/libstderred.dylib${DYLD_INSERT_LIBRARIES:+:DYLD_INSERT_LIBRARIES}"
     EOS
   end
 end
